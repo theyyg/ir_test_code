6307ce2b8556a82face90b3128803a88dfe6a900
Blaine Wood <theyyg@hotmail.com>
2022-12-20 16:27:04 -0700

    Clean-up and improved presentation
    Added handling of an hex string that is too large for an integer
    
    Added helper functions to print hex2int and the linked list
    
    Added a duplicate value to the linked list to show that only one value
    is removed

--- iR_Execute/iR_Execute.cpp
+++ iR_Execute/iR_Execute.cpp
@@ -1,9 +1,9 @@
 // iR_Execute.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
 #include <exception>
-
+#include <iostream>
+#include <string>
 
 /// <summary>
 /// Linked List Node with an integer value
@@ -20,36 +20,45 @@ struct node
 /// <param name="s">A C-string that contains a hexidecimal value of an ASCII character</param>
 /// <returns><para>Returns the integer value of the ASCII input</para>
 /// <para>Throws exception if the c-string is a nullptr</para>
-/// <para>Throws exception if the hexidecimal value is not valid. (e.g. "2G") </returns>
+/// <para>Throws exception if the hexidecimal value is not valid. (e.g. "2G") </para></returns>
 int hex2int(const char * s)
 {
-    int converted_hex = 0;
+    int convertedHex = 0;
 
     // Verify that the string has a valid pointer
-    if (s == nullptr)
+    if (s == 0)
     {
         // Return an non-ASCII value for an error code
         throw std::invalid_argument("hex2int: cstring is NULL");
     }
 
+    size_t stringSize = strlen(s);
+
     const char ZERO = '0';
     const char BIG_A = 'A';
     const char LITTLE_A = 'a';
-    size_t string_size = strlen(s);
-    long long hex_digit = 0;                      // Use -1 as an invalid digit
+    int hexDigit = 0;                      // Use -1 as an invalid digit
+    bool isValid = true;
     size_t i = 0;
 
     // Skip over "0x" if formatted in that way
-    if (string_size > 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
+    if (stringSize > 2 && s[0] == '0' && (s[1] == 'x' || s[1] == 'X'))
     {
         i = 2;
+    }
+
+    // Check for integer overflow (hex string is too big)
+    if ((stringSize > 10 && i == 2) ||
+        stringSize > 8)
+    {
+        throw std::invalid_argument("hex2int: argument string is too large for int conversion");
     }
 
     for (/*i is set prior*/; s[i] != '\0'; ++i)           // Traverse the string and convert 
     {
         // Shift the value up by one byte.
         // Unnecessary at i==0, but it's fast and prevents unecessary compares
-        hex_digit = hex_digit << 4;
+        hexDigit = hexDigit << 4;
 
         // Extract 0-9
         switch (s[i])
@@ -65,7 +74,7 @@ int hex2int(const char * s)
         case '8':
         case '9':
             // The integer value is the difference from the base ASCII value (0)
-            hex_digit += s[i] - ZERO;
+            hexDigit += s[i] - ZERO;
             break;
         case 'a':
         case 'b':
@@ -74,7 +83,7 @@ int hex2int(const char * s)
         case 'e':
         case 'f':
             // The integer value is the difference from the base ASCII value (a) + 0xA (or 10)
-            hex_digit += s[i] - LITTLE_A + 0xA;
+            hexDigit += s[i] - LITTLE_A + 0xA;
             break;
         case 'A':
         case 'B':
@@ -83,21 +92,21 @@ int hex2int(const char * s)
         case 'E':
         case 'F':
             // The integer value is the difference from the base ASCII value (A) + 0xA (or 10)
-            hex_digit += s[i] - BIG_A + 0xA;
+            hexDigit += s[i] - BIG_A + 0xA;
             break;
         case '\0':
             break;
         default:
-            hex_digit = -1;
+            isValid = false;
             break;
         }
 
-        // Poorly formed 
-        if (hex_digit == -1)
+        // Poorly formed string
+        if (!isValid)
             throw std::invalid_argument("hex2int: Invalid string format");
     }
 
-    return hex_digit;
+    return hexDigit;
 
 }
 
@@ -118,14 +127,12 @@ bool remove_recursive(int value, node ** h)
         return false;
     }
 
-    std::cout << (*h)->value << std::endl;
-
     // Satisfy the terminal case for recursion
     if ((*h)->value == value)
     {
-        node * next_node = (*h)->next;
+        node * nextNode = (*h)->next;
         (*h)->next = nullptr;
-        *h = next_node;
+        *h = nextNode;
         isNodeRemoved = true;
     }
     else
@@ -160,100 +167,190 @@ bool remove(int value, node ** h)
     // Handle root node removal
     if ((*h)->value == value)
     {
-        node * new_root = (*h)->next;
+        node * newRoot = (*h)->next;
         (*h)->next = nullptr;
-        *h = new_root;
+        *h = newRoot;
 
         return true;
     }
 
-    node * active_node = (*h);
-    for (node * next_node = active_node->next; active_node != nullptr; /* inc in loop */)
+    node * activeNode = (*h);
+    for (node * nextNode = activeNode->next; activeNode != nullptr; /* inc in loop */)
     {
         // Remove matching nodes
-        if (next_node != nullptr && next_node->value == value)
+        if (nextNode != nullptr && nextNode->value == value)
         { 
-            node * new_next = next_node->next;
-            active_node->next = new_next;
-            next_node->next = nullptr;
+            node * tempNext = nextNode->next;
+            activeNode->next = tempNext;
+            nextNode->next = nullptr;
             isNodeRemoved = true;
 
             // This could be remove to remove all nodes with the given value
             break;
         }
 
-        active_node = active_node->next;
-        if (active_node != nullptr)
+        activeNode = activeNode->next;
+        if (activeNode != nullptr)
         {
-            next_node = active_node->next;
+            nextNode = activeNode->next;
         }
     }
     return isNodeRemoved;
 }
 
+/// <summary>
+/// Prints the hex string and it's converted value
+/// - Catches any exceptions that occur during conversion
+/// </summary>
+/// <param name="hexString">A string containing the text representation of a hex value</param>
+void PrintHex2Int(const std::string & hexString)
+{
+    std::cout << '\"'
+        << hexString
+        << "\" = ";
+    try
+    {
+        std::cout << hex2int(hexString.c_str());
+    }
+    catch (std::invalid_argument & e)
+    {
+        std::cerr << std::endl
+            << "Error converting string to integer:"
+            << std::endl
+            << " > "
+            << e.what();
+    }
+    std::cout << std::endl;
+}
+
+
+void PrintList(node * activeNode, bool isVertical = false, bool isRoot = true)
+{
+    if (activeNode == nullptr) 
+    { 
+        return; 
+    }
+
+    if (isRoot) 
+    { 
+        std::cout << '*'; 
+    }
+
+    std::cout << activeNode->value;
+
+    if (activeNode->next != nullptr)
+    {
+        if (isVertical)
+        {
+            std::cout << std::endl << " |" << std::endl;
+        }
+        else
+        {
+            std::cout << " -> ";
+        }
+    }
+
+    PrintList(activeNode->next, isVertical, false);
+
+    if (isRoot)
+    {
+        std::cout << std::endl;
+    }
+}
+
+void PrintRemovalSuccess(int value, bool isRemoved)
+{
+    if (isRemoved)
+    {
+        std::cout << value << " has been removed";
+    }
+    else
+    {
+        std::cout << value << " was not removed";
+    }
+    std::cout << std::endl;
+}
 
 int main()
 {
     // Test and demonstrate the hex conversion
-    try
-    {
-        std::cout << "Testing Hex Conversion" << std::endl;
-        // Test Limits
-        std::cout << "\"00\" = ";
-        std::cout << hex2int("00") << std::endl;
-        // Test Valid
-        std::cout << "\"FFFFFFFF\" = ";
-        std::cout << hex2int("FFFFFFFF") << std::endl;
-        // Test Alternate Format
-        std::cout << "\"0xD00d\" = ";
-        std::cout << hex2int("0xD00d") << std::endl;
-        // Test Invalid
-        std::cout << "\"GOLF\" = ";
-        std::cout << hex2int("GOLF") << std::endl;
-    }
-    catch (std::invalid_argument & e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    std::cout <<  "== Testing Hex Conversion ==" << std::endl;
+    // Test Valid
+    std::cout << std::endl << "Valid Hex:" << std::endl;
+    PrintHex2Int("7");
+    PrintHex2Int("23");
+    PrintHex2Int("C");
+    // Test Limits
+    std::cout << std::endl << "Integer Limits:" << std::endl;
+    PrintHex2Int("00");
+    PrintHex2Int("FFFFFFFF");
+    // Test Alternate Format
+    std::cout << std::endl << "Alternate format:" << std::endl;
+    PrintHex2Int("D00d");
+    PrintHex2Int("0xBAD");
+    PrintHex2Int("0xbeef");
+    // Test Too Big
+    std::cout << std::endl << "Too Big:" << std::endl;
+    PrintHex2Int("1234567890ABCDEF");
+    // Test Invalid
+    std::cout << std::endl << "Invalid string:" << std::endl;
+    PrintHex2Int("GOLF");
+    // Test Bad Pointer
+    std::cout << std::endl << "Null Pointer:" << std::endl;
+    try { hex2int(nullptr); }
+    catch (std::invalid_argument& e) { std::cerr << e.what() << std::endl; }
+
+    std::cout << std::endl;
 
     // Test and demonstrate the linked list removal
+    std::cout << "== Testing Linked List Removal ==" << std::endl;
     const unsigned int NODE_COUNT = 10;
     node nodes[NODE_COUNT];
-    node * root_node = &(nodes[0]);
+    node * rootNode = &(nodes[0]);
 
-    // Populate the linked list with 
-    node * active_node = root_node;
+    // Populate the linked list with values 0-10
+    node * activeNode = rootNode;
     for (unsigned int i = 0; i < NODE_COUNT; ++i)
     {
-        active_node->next = nullptr;
-        active_node->value = i;
+        activeNode->next = nullptr;
+        activeNode->value = i;
         if (i != (NODE_COUNT - 1))
         {
-            active_node->next = &(nodes[i + 1]);
+            activeNode->next = &(nodes[i + 1]);
         }
 
-        active_node = active_node->next;
+        activeNode = activeNode->next;
     }
+
+    std::cout << std::endl << "Populate the list with consecutive integers (0-" << NODE_COUNT << ")" << std::endl;
+    PrintList(rootNode);
+
+    // Let's change node[8] to have a value of 3 so that there are duplicates
+    nodes[8].value = 3;
+    std::cout << std::endl << "Set the 8th node's value to 3 for a duplicate" << std::endl;
+    PrintList(rootNode);
 
     // Remove nodes
-    remove(0, &root_node);
-    remove_recursive(3, &root_node);
-    bool isRemoved = remove(7, &root_node);
-    remove(100, &root_node);
+    bool isRemoved;
+    isRemoved = remove(0, &rootNode);
+    std::cout << std::endl << "Remove node with value 0" << std::endl;
+    PrintList(rootNode);
+    PrintRemovalSuccess(0, isRemoved);
 
-    // Display the results
-    std::cout << "The linked list values: " << std::endl;
-    for (active_node = root_node; active_node != nullptr; active_node = active_node->next)
-    {
-        std::cout << "  " << active_node->value << std::endl;
-    }
+    isRemoved = remove_recursive(3, &rootNode);
+    std::cout << std::endl << "Remove node with value 3" << std::endl;
+    PrintList(rootNode);
+    PrintRemovalSuccess(3, isRemoved);
 
-    std::cout << "The node with value of 7 was ";
-    if (!isRemoved)
-    {
-        std::cout << "not ";
-    }
-    std::cout << "removed.";
+    isRemoved = remove(7, &rootNode);
+    std::cout << std::endl << "Remove node with value 7" << std::endl;
+    PrintList(rootNode);
+    PrintRemovalSuccess(7, isRemoved);
+    
+    remove(100, &rootNode);
+    std::cout << std::endl << "Remove node with value 100" << std::endl;
+    PrintList(rootNode);
+    PrintRemovalSuccess(100, isRemoved);
 
     return 0;
 }
